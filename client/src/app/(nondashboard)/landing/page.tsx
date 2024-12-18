2cfb78cfb38404f068bbeab58d228c386f37d0b0
GayanGamage96 <gayangamagercg1996@gmail.com>
2024-12-18 19:59:40 +0530

    landing page completed

--- client/src/app/(nondashboard)/landing/page.tsx
+++ client/src/app/(nondashboard)/landing/page.tsx
@@ -6,6 +6,9 @@ import Image from "next/image";
 import { useCarousel } from "@/hooks/useCarousel";
 import { landingTags } from "@/constants";
 import { Skeleton } from "@/components/ui/skeleton";
+import { useGetCoursesQuery } from "@/state/api";
+import CourseCardSearch from "@/components/CourseCardSearch";
+import { useRouter } from "next/navigation";
 
 const LoadingSkeleton = () => {
   return (
@@ -18,28 +21,35 @@ const LoadingSkeleton = () => {
           <Skeleton className="landing-skeleton__button" />
         </div>
         <Skeleton className="landing-skeleton__hero-image" />
-
       </div>
       <div className="landing-skeleton__featured">
-      <Skeleton className="landing-skeleton__featured-title" />
-      <Skeleton className="landing-skeleton__featured-description" />
-      <div className="landing-skeleton__tags">
-      {[1,2,3,4,5,6].map((_,index)=>(
-        <Skeleton key={index} className="landing-skeleton__tag" />
-      ))}
-      </div>
-      <div className="landing-skeleton__courses">
-      {[1,2,3,4].map((_,index)=>(
-        <Skeleton key={index} className="landing-skeleton__course-card" />
-      ))}
-      </div>
+        <Skeleton className="landing-skeleton__featured-title" />
+        <Skeleton className="landing-skeleton__featured-description" />
+        <div className="landing-skeleton__tags">
+          {[1, 2, 3, 4, 5, 6].map((_, index) => (
+            <Skeleton key={index} className="landing-skeleton__tag" />
+          ))}
+        </div>
+        <div className="landing-skeleton__courses">
+          {[1, 2, 3, 4].map((_, index) => (
+            <Skeleton key={index} className="landing-skeleton__course-card" />
+          ))}
+        </div>
       </div>
     </div>
   );
 };
 
 const LandingPage = () => {
+  const router = useRouter();
   const currentImage = useCarousel({ totalImages: 3 });
+  const { data: courses, isLoading, isError } = useGetCoursesQuery({});
+  console.log("courses:", courses);
+  //console.log(courses?.image);
+  const handleCourseClick = (courseId: string) => {
+    router.push(`/search?id=${courseId}`);
+  };
+  if(isLoading) return <LoadingSkeleton/>
   return (
     <motion.div
       initial={{ opacity: 0 }}
@@ -101,7 +111,23 @@ const LandingPage = () => {
             </span>
           ))}
         </div>
-        <div className="landing__courses">{/* COURSES DISPLAY */}</div>
+        <div className="landing__courses">
+          {courses &&
+            courses.slice(0, 4).map((course, index) => (
+              <motion.div
+                key={course.courseId}
+                initial={{ y: 50, opacity: 0 }}
+                whileInView={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.5, delay: index * 0.2 }}
+                viewport={{ amount: 0.4 }}
+              >
+                <CourseCardSearch
+                  course={course}
+                  onClick={() => handleCourseClick(course.courseId)}
+                />
+              </motion.div>
+            ))}
+        </div>
       </motion.div>
     </motion.div>
   );
