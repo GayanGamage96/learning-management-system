db9a2cb1886ef252575f501c85b628c1eb3d040d
GayanGamage96 <gayangamagercg1996@gmail.com>
2024-12-28 22:15:40 +0530

    backend- create transactions api

--- server/src/controllers/transactionController.ts
+++ server/src/controllers/transactionController.ts
@@ -1,6 +1,9 @@
 import Stripe from "stripe";
 import dotenv from "dotenv";
 import { Request, Response } from "express";
+import Course from "../models/courseModel";
+import Transaction from "../models/transactionModel";
+import UserCourseProgress from "../models/userCourseProgressModel";
 
 dotenv.config();
 
@@ -39,5 +42,87 @@ export const createStripePaymentIntent = async (
     res
       .status(500)
       .json({ message: "Error creating stripe payment intent", error });
+  }
+};
+
+export const createTransaction = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const { userId, courseId, transactionId, amount, paymentProvider } = req.body;
+  try {
+    //1.get course info
+    const course = await Course.get(courseId);
+    const newTransaction = new Transaction({
+      dateTime: new Date().toISOString(),
+      userId,
+      courseId,
+      transactionId,
+      amount,
+      paymentProvider,
+    });
+    await newTransaction.save();
+
+    //3.create initial course progress
+    const initialProgress = new UserCourseProgress({
+      userId,
+      courseId,
+      enrollmentDate: new Date().toISOString(),
+      overallProgress: 0,
+      sections: course.sections.map((section: any) => ({
+        sectionId: section.sectionId,
+        chapters: section.chapters.map((chapter: any) => ({
+          chapterId: chapter.chapterId,
+          completed: false,
+        })),
+      })),
+      lastAccessedTimestamp: new Date().toISOString(),
+    });
+    await initialProgress.save();
+
+    // 4. Add enrollment to relevant course
+    await Course.update(
+      { courseId },
+      {
+        $ADD: {
+          enrollments: [{ userId }],
+        },
+      }
+    );
+
+    res.json({
+      message: "Purchase Course successfully",
+      data: {
+        transaction: newTransaction,
+        courseProgress: initialProgress,
+      },
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error creating transaction and enrollments", error });
+  }
+};
+
+export const listTransactions = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const { userId } = req.query;
+
+  try {
+    const transactions = userId
+      ? await Transaction.query("userId").eq(userId).exec()
+      : await Transaction.scan().exec();
+
+    res.json({
+      message: "Transactions retrieved successfully",
+      data: transactions,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error retrieving transaction",
+      error,
+    });
   }
 };
