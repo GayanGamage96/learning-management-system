3e9be92e4fff086b2d66fefdeea88bef0c3b4fde
GayanGamage96 <gayangamagercg1996@gmail.com>
2025-01-01 09:26:12 +0530

    add courses queries to fetch

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -57,6 +57,7 @@ export const api = createApi({
   reducerPath: "api",
   tagTypes: ["Courses", "Users"],
   endpoints: (build) => ({
+    //  USER CLERK
     updateUser: build.mutation<User, Partial<User> & { userId: string }>({
       query: ({ userId, ...updatedUser }) => ({
         url: `users/clerk/${userId}`,
@@ -65,6 +66,8 @@ export const api = createApi({
       }),
       invalidatesTags: ["Users"],
     }),
+
+    //  COURSES
     getCourses: build.query<Course[], { category?: string }>({
       query: ({ category }) => ({
         url: "courses",
@@ -72,6 +75,7 @@ export const api = createApi({
       }),
       providesTags: ["Courses"],
     }),
+
     getCourse: build.query<Course, string>({
       query: (courseId) => `courses/${courseId}`,
       providesTags: (result, error, courseId) => [
@@ -79,8 +83,43 @@ export const api = createApi({
       ],
     }),
 
-    getTransactions:build.query<Transaction[],string>({
-      query:(userId)=>`transactions?userId=${userId}`
+    createCourse: build.mutation<
+      Course,
+      { teacherId: string; teacherName: string }
+    >({
+      query: (body) => ({
+        url: `courses`,
+        method: "POST",
+        body,
+      }),
+      invalidatesTags: ["Courses"],
+    }),
+
+    updateCourse: build.mutation<
+      Course,
+      { courseId: string; formData: FormData }
+    >({
+      query: ({ courseId, formData }) => ({
+        url: `courses/${courseId}`,
+        method: "PUT",
+        body: formData,
+      }),
+      invalidatesTags: (result, error, { courseId }) => [
+        { type: "Courses", id: courseId },
+      ],
+    }),
+
+    deleteCourse: build.mutation<{ message: string }, string>({
+      query: (courseId) => ({
+        url: `courses/${courseId}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["Courses"],
+    }),
+
+    // TRANSACTIONS
+    getTransactions: build.query<Transaction[], string>({
+      query: (userId) => `transactions?userId=${userId}`,
     }),
     createStripePaymentIntent: build.mutation<
       { clientSecret: string },
@@ -107,7 +146,10 @@ export const {
   useUpdateUserMutation,
   useGetCoursesQuery,
   useGetCourseQuery,
+  useCreateCourseMutation,
+  useUpdateCourseMutation,
+  useDeleteCourseMutation,
   useCreateStripePaymentIntentMutation,
   useCreateTransactionMutation,
-  useGetTransactionsQuery
+  useGetTransactionsQuery,
 } = api;
